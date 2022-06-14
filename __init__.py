3fbc5ebbaaf6a5d6aada69dc1539d9462aa94f6b
C Hamilton <hamiltoncj@users.noreply.github.com>
2022-06-14 18:24:51 +0000

    Remove h3 plugin check

--- __init__.py
+++ __init__.py
@@ -1,19 +1,3 @@
-try:
-    import h3
-    utils.H3_INSTALLED = True
-except Exception:
-    import os
-    import site
-    import platform
-    if platform.system() == 'Windows':
-        site.addsitedir(os.path.abspath(os.path.dirname(__file__) + '/libs'))
-    try:
-        import h3
-        from . import utils
-        utils.H3_INSTALLED = True
-    except Exception:
-        pass
-
 def classFactory(iface):
     from .densityanalysis import DensityAnalysis
     return DensityAnalysis(iface)
