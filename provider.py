8e1a22f47f33b2220bdfcb2355ee2e49c4cb9efc
C Hamilton <hamiltoncj@users.noreply.github.com>
2023-02-02 19:55:45 +0000

    Add new processing algorithms.

--- provider.py
+++ provider.py
@@ -5,10 +5,14 @@ from .randomstyle import RandomStyleAlgorithm
 from .graduatedstyle import GraduatedStyleAlgorithm
 from .densitygrid import StyledDensityGridAlgorithm
 from .geohashdensity import GeohashDensityAlgorithm
+from .geohashmultidensity import GeohashMultiLayerDensityAlgorithm
 from .geohashdensitymap import GeohashDensityMapAlgorithm
+from .geohashmultidensitymap import GeohashMultiLayerDensityMapAlgorithm
 from .h3grid import H3GridAlgorithm
 from .h3density import H3DensityAlgorithm
+from .h3multidensity import H3MultiLayerDensityAlgorithm
 from .h3densitymap import H3DensityMapAlgorithm
+from .h3multidensitymap import H3MultiLayerDensityMapAlgorithm
 from .polygondensity import PolygonRasterDensityAlgorithm
 from .styledpolygondensity import StyledPolygonRasterDensityAlgorithm
 from .rasterstyle import RasterStyleAlgorithm
@@ -24,10 +28,14 @@ class DensityAnalysisProvider(QgsProcessingProvider):
         self.addAlgorithm(GraduatedStyleAlgorithm())
         self.addAlgorithm(StyledDensityGridAlgorithm())
         self.addAlgorithm(GeohashDensityAlgorithm())
+        self.addAlgorithm(GeohashMultiLayerDensityAlgorithm())
         self.addAlgorithm(GeohashDensityMapAlgorithm())
+        self.addAlgorithm(GeohashMultiLayerDensityMapAlgorithm())
         self.addAlgorithm(H3GridAlgorithm())
         self.addAlgorithm(H3DensityAlgorithm())
+        self.addAlgorithm(H3MultiLayerDensityAlgorithm())
         self.addAlgorithm(H3DensityMapAlgorithm())
+        self.addAlgorithm(H3MultiLayerDensityMapAlgorithm())
         self.addAlgorithm(RasterStyleAlgorithm())
         self.addAlgorithm(PolygonRasterDensityAlgorithm())
         self.addAlgorithm(StyledPolygonRasterDensityAlgorithm())
