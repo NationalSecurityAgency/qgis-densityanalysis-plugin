fe25b3440f30d4e7735b76ee3e7aab3163f4923f
C Hamilton <hamiltoncj@users.noreply.github.com>
2023-02-02 19:42:59 +0000

    Add multi-layer geohash density algorithms

---
+++ geohashmultidensity.py
@@ -0,0 +1,203 @@
+import os
+from qgis.PyQt.QtGui import QIcon
+from qgis.PyQt.QtCore import QVariant
+from qgis.core import Qgis, QgsWkbTypes, QgsFields, QgsField, QgsCoordinateTransform, QgsCoordinateReferenceSystem, QgsRectangle, QgsFeature, QgsGeometry, QgsProject
+
+from qgis.core import (
+    QgsProcessing,
+    QgsProcessingAlgorithm,
+    QgsProcessingException,
+    QgsProcessingParameterMultipleLayers,
+    QgsProcessingParameterField,
+    QgsProcessingParameterNumber,
+    QgsProcessingParameterFeatureSink
+    )
+import processing
+
+from . import geohash
+
+class GeohashMultiLayerDensityAlgorithm(QgsProcessingAlgorithm):
+
+    def initAlgorithm(self, config=None):
+        self.addParameter(
+            QgsProcessingParameterMultipleLayers('INPUT', 'Input point vector layers', QgsProcessing.TypeVectorPoint)
+        )
+        param = QgsProcessingParameterNumber('RESOLUTION', 'Geohash resolution',
+                type=QgsProcessingParameterNumber.Integer, minValue=1, defaultValue=6, maxValue=12, optional=False)
+        if Qgis.QGIS_VERSION_INT >= 31600:
+            param.setHelp(
+                '''
+                The resolution level of the grid, as defined in the geohash standard.
+                <br>
+                <table>
+                  <tr>
+                    <th>Resolution<br>Level</th>
+                    <th>Approximate<br>Dimensions</th>
+                  </tr>
+                  <tr>
+                    <td style="text-align: center">1</td>
+                    <td style="text-align: center">≤ 5,000km X 5,000km</td>
+                  </tr>
+                  <tr>
+                    <td style="text-align: center">2</td>
+                    <td style="text-align: center">≤ 1,250km X 625km</td>
+                  </tr>
+                  <tr>
+                    <td style="text-align: center">3</td>
+                    <td style="text-align: center">≤ 156km X 156km</td>
+                  </tr>
+                  <tr>
+                    <td style="text-align: center">4</td>
+                    <td style="text-align: center">≤ 39.1km X 19.5km</td>
+                  </tr>
+                  <tr>
+                    <td style="text-align: center">5</td>
+                    <td style="text-align: center">≤ 4.89km X 4.89km</td>
+                  </tr>
+                  <tr>
+                    <td style="text-align: center">6</td>
+                    <td style="text-align: center">≤ 1.22km X 0.61km</td>
+                  </tr>
+                  <tr>
+                    <td style="text-align: center">7</td>
+                    <td style="text-align: center">≤ 153m X 153m</td>
+                  </tr>
+                  <tr>
+                    <td style="text-align: center">8</td>
+                    <td style="text-align: center">≤ 38.2m X 19.1m</td>
+                  </tr>
+                  <tr>
+                    <td style="text-align: center">9</td>
+                    <td style="text-align: center">≤ 4.77m X 4.77m</td>
+                  </tr>
+                  <tr>
+                    <td style="text-align: center">10</td>
+                    <td style="text-align: center">≤ 1.19m X 0.596m</td>
+                  </tr>
+                  <tr>
+                    <td style="text-align: center">11</td>
+                    <td style="text-align: center">≤ 149mm X 149mm</td>
+                  </tr>
+                  <tr>
+                    <td style="text-align: center">12</td>
+                    <td style="text-align: center">≤ 37.2mm X 18.6mm</td>
+                  </tr>
+                </table>
+                '''
+            )
+        self.addParameter(param)
+        self.addParameter(
+            QgsProcessingParameterField(
+                'WEIGHT',
+                'Weight field',
+                parentLayerParameterName='INPUT',
+                type=QgsProcessingParameterField.Numeric,
+                optional=True)
+        )
+        self.addParameter(
+            QgsProcessingParameterFeatureSink('OUTPUT', 'Output geohash density map',
+                type=QgsProcessing.TypeVectorPolygon, createByDefault=True, defaultValue=None)
+        )
+
+    def processAlgorithm(self, parameters, context, feedback):
+        layer_list = self.parameterAsLayerList(parameters, 'INPUT', context)
+        if layer_list is None or len(layer_list) == 0:
+            raise QgsProcessingException('No point layers were selected.')
+        resolution = self.parameterAsInt(parameters, 'RESOLUTION', context)
+        if 'WEIGHT' in parameters and parameters['WEIGHT']:
+            use_weight = True
+            weight_field = self.parameterAsString(parameters, 'WEIGHT', context)
+        else:
+            use_weight = False
+        
+        epsg4326 = QgsCoordinateReferenceSystem("EPSG:4326")
+        fields = QgsFields()
+        fields.append(QgsField('ID', QVariant.Int))
+        fields.append(QgsField('GEOHASH', QVariant.String))
+        fields.append(QgsField('NUMPOINTS', QVariant.Double))
+        (sink, dest_id) = self.parameterAsSink(
+            parameters, 'OUTPUT',
+            context, fields, QgsWkbTypes.Polygon, epsg4326)
+
+        ghash = {}
+        num_layers = len(layer_list)
+        cumulative = 0
+        incremental = 85 / num_layers
+        for layer in layer_list:
+            src_crs = layer.sourceCrs()
+            
+            if src_crs != epsg4326:
+                transform = QgsCoordinateTransform(src_crs, epsg4326, QgsProject.instance())
+
+            total = incremental / layer.featureCount() if layer.featureCount() else 0
+
+            iterator = layer.getFeatures()
+            if use_weight:
+                for cnt, feature in enumerate(iterator):
+                    if feedback.isCanceled():
+                        break
+                    try:
+                        pt = feature.geometry().asPoint()
+                        if src_crs != epsg4326:
+                            pt = transform.transform(pt)
+                        h = geohash.encode(pt.y(), pt.x(), resolution)
+                        weight = feature[weight_field]
+                        if h in ghash:
+                            ghash[h] += weight
+                        else:
+                            ghash[h] = weight
+                    except Exception:
+                        pass
+                    if cnt % 1000 == 0:
+                        feedback.setProgress(int(cnt * total + cumulative))
+            else:
+                for cnt, feature in enumerate(iterator):
+                    if feedback.isCanceled():
+                        break
+                    try:
+                        pt = feature.geometry().asPoint()
+                        if src_crs != epsg4326:
+                            pt = transform.transform(pt)
+                        h = geohash.encode(pt.y(), pt.x(), resolution)
+                        if h in ghash:
+                            ghash[h] += 1
+                        else:
+                            ghash[h] = 1
+                    except Exception:
+                        pass
+                    if cnt % 1000 == 0:
+                        feedback.setProgress(int(cnt * total + cumulative))
+            cumulative += incremental
+
+        if len(ghash) == 0:
+            return {}
+        total = 15 / len(ghash)
+        for cnt, key in enumerate(ghash.keys()):
+            val = ghash[key]
+            lat1, lat2, lon1, lon2 = geohash.decode_extent(key)
+            rect = QgsRectangle(lon1, lat1, lon2, lat2)
+            f = QgsFeature()
+            f.setGeometry(QgsGeometry.fromRect(rect))
+            f.setAttributes([cnt, key, val])
+            sink.addFeature(f)
+            if cnt % 100 == 0:
+                feedback.setProgress(int(cnt * total)+85)
+        return {'OUTPUT': dest_id}
+
+    def group(self):
+        return 'Geohash density'
+
+    def groupId(self):
+        return 'geohashdensity'
+
+    def name(self):
+        return 'geohashmultidensity'
+
+    def displayName(self):
+        return 'Geohash multi-layer density grid'
+
+    def icon(self):
+        return QIcon(os.path.join(os.path.dirname(__file__), 'icons/geohashmultidensity.svg'))
+
+    def createInstance(self):
+        return GeohashMultiLayerDensityAlgorithm()
---
+++ geohashmultidensitymap.py
@@ -0,0 +1,209 @@
+import os
+from qgis.PyQt.QtGui import QIcon
+from qgis.core import Qgis
+
+from qgis.core import (
+    QgsProcessing,
+    QgsProcessingAlgorithm,
+    QgsProcessingParameterBoolean,
+    QgsProcessingParameterEnum,
+    QgsProcessingParameterField,
+    QgsProcessingParameterMultipleLayers,
+    QgsProcessingParameterNumber,
+    QgsProcessingParameterString,
+    QgsProcessingParameterDefinition,
+    QgsProcessingParameterFeatureSink
+    )
+import processing
+
+from . import geohash
+from .settings import settings, COLOR_RAMP_MODE
+
+class GeohashMultiLayerDensityMapAlgorithm(QgsProcessingAlgorithm):
+
+    def initAlgorithm(self, config=None):
+        self.addParameter(
+            QgsProcessingParameterMultipleLayers('INPUT', 'Input point vector layers', QgsProcessing.TypeVectorPoint)
+        )
+        param = QgsProcessingParameterNumber('RESOLUTION', 'Geohash resolution',
+                type=QgsProcessingParameterNumber.Integer, minValue=1, defaultValue=6, maxValue=12, optional=False)
+        if Qgis.QGIS_VERSION_INT >= 31600:
+            param.setHelp(
+                '''
+                The resolution level of the grid, as defined in the geohash standard.
+                <br>
+                <table>
+                  <tr>
+                    <th>Resolution<br>Level</th>
+                    <th>Approximate<br>Dimensions</th>
+                  </tr>
+                  <tr>
+                    <td style="text-align: center">1</td>
+                    <td style="text-align: center">≤ 5,000km X 5,000km</td>
+                  </tr>
+                  <tr>
+                    <td style="text-align: center">2</td>
+                    <td style="text-align: center">≤ 1,250km X 625km</td>
+                  </tr>
+                  <tr>
+                    <td style="text-align: center">3</td>
+                    <td style="text-align: center">≤ 156km X 156km</td>
+                  </tr>
+                  <tr>
+                    <td style="text-align: center">4</td>
+                    <td style="text-align: center">≤ 39.1km X 19.5km</td>
+                  </tr>
+                  <tr>
+                    <td style="text-align: center">5</td>
+                    <td style="text-align: center">≤ 4.89km X 4.89km</td>
+                  </tr>
+                  <tr>
+                    <td style="text-align: center">6</td>
+                    <td style="text-align: center">≤ 1.22km X 0.61km</td>
+                  </tr>
+                  <tr>
+                    <td style="text-align: center">7</td>
+                    <td style="text-align: center">≤ 153m X 153m</td>
+                  </tr>
+                  <tr>
+                    <td style="text-align: center">8</td>
+                    <td style="text-align: center">≤ 38.2m X 19.1m</td>
+                  </tr>
+                  <tr>
+                    <td style="text-align: center">9</td>
+                    <td style="text-align: center">≤ 4.77m X 4.77m</td>
+                  </tr>
+                  <tr>
+                    <td style="text-align: center">10</td>
+                    <td style="text-align: center">≤ 1.19m X 0.596m</td>
+                  </tr>
+                  <tr>
+                    <td style="text-align: center">11</td>
+                    <td style="text-align: center">≤ 149mm X 149mm</td>
+                  </tr>
+                  <tr>
+                    <td style="text-align: center">12</td>
+                    <td style="text-align: center">≤ 37.2mm X 18.6mm</td>
+                  </tr>
+                </table>
+                '''
+            )
+        self.addParameter(param)
+        if Qgis.QGIS_VERSION_INT >= 32200:
+            ramp_name_param = QgsProcessingParameterString('RAMP_NAMES', 'Select color ramp', defaultValue=settings.defaultColorRamp())
+            ramp_name_param.setMetadata( {'widget_wrapper': {'value_hints': settings.ramp_names } } )
+        else:
+            ramp_name_param = QgsProcessingParameterEnum(
+                'RAMP_NAMES',
+                'Select color ramp',
+                options=settings.ramp_names,
+                defaultValue=settings.defaultColorRampIndex(),
+                optional=False)
+        self.addParameter(ramp_name_param)
+        self.addParameter(
+            QgsProcessingParameterBoolean(
+                'INVERT',
+                'Invert color ramp',
+                False,
+                optional=False)
+        )
+
+        param = QgsProcessingParameterField(
+            'WEIGHT',
+            'Weight field',
+            parentLayerParameterName='INPUT',
+            type=QgsProcessingParameterField.Numeric,
+            optional=True)
+        param.setFlags(param.flags() | QgsProcessingParameterDefinition.FlagAdvanced)
+        self.addParameter(param)
+        param = QgsProcessingParameterNumber(
+            'CLASSES',
+            'Number of gradient colors',
+            QgsProcessingParameterNumber.Integer,
+            defaultValue=settings.num_ramp_classes,
+            minValue=2,
+            optional=False)
+        param.setFlags(param.flags() | QgsProcessingParameterDefinition.FlagAdvanced)
+        self.addParameter(param)
+        param = QgsProcessingParameterEnum(
+            'COLOR_RAMP_MODE',
+            'Color ramp mode',
+            options=COLOR_RAMP_MODE,
+            defaultValue=settings.color_ramp_mode,
+            optional=False)
+        param.setFlags(param.flags() | QgsProcessingParameterDefinition.FlagAdvanced)
+        self.addParameter(param)
+        param = QgsProcessingParameterBoolean(
+            'NO_OUTLINE',
+            'No feature outlines',
+            True,
+            optional=False)
+        param.setFlags(param.flags() | QgsProcessingParameterDefinition.FlagAdvanced)
+        self.addParameter(param)
+
+        self.addParameter(
+            QgsProcessingParameterFeatureSink('OUTPUT', 'Output geohash density map',
+                type=QgsProcessing.TypeVectorPolygon, createByDefault=True, defaultValue=None)
+        )
+
+    def processAlgorithm(self, parameters, context, feedback):
+        resolution = self.parameterAsInt(parameters, 'RESOLUTION', context)
+        num_classes = self.parameterAsInt(parameters, 'CLASSES', context)
+        if 'WEIGHT' in parameters and parameters['WEIGHT']:
+            use_weight = True
+            weight_field = self.parameterAsString(parameters, 'WEIGHT', context)
+        else:
+            use_weight = False
+        if Qgis.QGIS_VERSION_INT >= 32200:
+            # In this case ramp_name will be the name
+            ramp_name = self.parameterAsString(parameters, 'RAMP_NAMES', context)
+        else:
+            # In this case ramp_name will be an index into ramp_names
+            ramp_name = self.parameterAsEnum(parameters, 'RAMP_NAMES', context)
+        ramp_mode = self.parameterAsInt(parameters, 'COLOR_RAMP_MODE', context)
+        no_outline = self.parameterAsBool(parameters, 'NO_OUTLINE', context)
+        invert = self.parameterAsBool(parameters, 'INVERT', context)
+        
+        results = {}
+        outputs = {}
+
+        alg_params = {
+            'INPUT':  parameters['INPUT'],
+            'RESOLUTION': resolution,
+            'OUTPUT': parameters['OUTPUT']
+        }
+        if use_weight:
+            alg_params['WEIGHT'] = weight_field
+        outputs['CreateGrid'] = processing.run('densityanalysis:geohashmultidensity', alg_params, context=context, feedback=feedback, is_child_algorithm=True)
+        results['OUTPUT'] = outputs['CreateGrid']['OUTPUT']
+
+        # Apply a graduated style
+        alg_params = {
+            'NO_OUTLINE': no_outline,
+            'INVERT': invert,
+            'CLASSES': num_classes,
+            'GROUP_FIELD': 'NUMPOINTS',
+            'INPUT': outputs['CreateGrid']['OUTPUT'],
+            'MODE': ramp_mode,
+            'RAMP_NAMES': ramp_name
+        }
+        processing.run('densityanalysis:gratuatedstyle', alg_params, context=context, feedback=feedback, is_child_algorithm=False)
+        return results
+
+    def group(self):
+        return 'Geohash density'
+
+    def groupId(self):
+        return 'geohashdensity'
+
+    def name(self):
+        return 'geohashmultidensitymap'
+
+    def displayName(self):
+        return 'Styled geohash multi-layer density map'
+
+    def icon(self):
+        return QIcon(os.path.join(os.path.dirname(__file__), 'icons/ml_geohash.png'))
+
+    def createInstance(self):
+        return GeohashMultiLayerDensityMapAlgorithm()
