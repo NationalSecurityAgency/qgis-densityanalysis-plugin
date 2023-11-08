c87b415fb9973b4552861cc180e8a9ec88d0af6b
C Hamilton <hamiltoncj@users.noreply.github.com>
2023-11-08 14:47:18 +0000

    Add license to each file.

--- utils.py
+++ utils.py
@@ -1,3 +1,13 @@
+"""
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+"""
 h3InstallString = '''
     <p>
       To create H3 density maps you will need to install the H3 Python Library (<a href="https://h3geo.org/">https://h3geo.org/</a>).<br><br>
